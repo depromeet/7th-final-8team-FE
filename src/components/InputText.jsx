32fe563877cd19250343d87e4996987e360c2ceb
girin-dev <kw8799@gmail.com>
2020-01-02 17:52:39 +0900

    컴포넌트수정. 주석추가,가독성 위한 코드재배치, 폰트사이즈 수정

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import {darken, lighten} from 'polished';
 
-
+// 라벨 사이즈
 const labelSizes = {
   large: {fontSize:'1rem',},
   medium: {fontSize:'0.875rem',},
@@ -14,18 +14,32 @@ const labelSizesStyles = css`
 		font-size: ${labelSizes[size].fontSize};
   `}
 `;
+// 라벨 컴포넌트
+const StyledLabel = styled.label`
+  display:block;
+  ${labelSizesStyles}
+  margin-left:1rem;
+  margin-right:1rem;
+  & + & {
+		margin-top: 0.5rem;
+	}
+`;
+
+// 입력창 사이즈
 const sizes = {
   large: {height:'3rem',fontSize:'1.25rem',},
   medium: {height:'2.25rem',fontSize:'1rem',},
-  small: {height:'1.75rem',fontSize:'0.875rem',},
+  small: {height:'1.75rem',fontSize:'0.875rem'},
 };
 const sizeStyles = css`
   /* 크기 */
   ${({ size }) => css`
 		height:${sizes[size].height};
-		font-size: ${sizes[size].fontSize};
+    font-size: ${sizes[size].fontSize};
+    line-height:${sizes[size].fontSize};
   `}
 `;
+// 입력창 컴포넌트
 const StyledInput = styled.input.attrs({
   type: 'text'
 })`
@@ -33,27 +47,26 @@ const StyledInput = styled.input.attrs({
   width:100%;
   box-sizing:border-box;
   display: block;
-  border: 1px solid #242729;
   outline:none;
+  border: 1px solid #242729;
   border-radius: 4px;
   color: #242729;
   font-weight: bold;
-  margin-top:0.1rem
-  padding-left: 1rem;
-  padding-right: 1rem;
+  margin-top:0.1rem;
+  padding:0 1rem 0 1rem;
+  vertical-align:bottom;
+  &::placeholder {
+    color: 	#A9A9A9;
+  }
   ${sizeStyles}
 `;
 
-const StyledLabel = styled.label`
-  display:block;
-  ${labelSizesStyles}
-`;
 
-function InputText({placeholder, label, ...rest}) {
+function InputText({placeholder, label, size, ...rest}) {
   return (
-    <StyledLabel {...rest}>
+    <StyledLabel size={size}>
       {label}
-      <StyledInput placeholder={placeholder} {...rest}/>
+      <StyledInput placeholder={placeholder} size={size} {...rest}/>
     </StyledLabel>
   )
 }
