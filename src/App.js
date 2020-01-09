4ee94e9b3dee3007ad7ae60b81dab2e6226396d6
yongseongjeon <yongseong.jeon95@gmail.com>
2020-01-09 20:41:49 +0900

    feat #26 - 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Details from './pages/Details';
 
 function App() {
   return (
-    <div className="App" />
+    <BrowserRouter>
+      <Route path="/" component={Home}/>
+      <Route path="/datails" component={Details}/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Modal from 'components/Modal';
+
+function Details() {
+  return (
+    <Modal />
+  );
+}
+
+export default Details;
