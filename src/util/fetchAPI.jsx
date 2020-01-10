5b39147c5263b2efdff992afd80abb31888fa532
girin-dev <kw8799@gmail.com>
2020-01-10 13:34:43 +0900

    커스텀Hook수정. API 통신을 위한 FetchAPI(method,URL,payload) 커스텀 Hook 개발

--- src/util/fetchAPI.jsx
+++ src/util/fetchAPI.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import { useEffect, useReducer } from 'react';
+import axios from 'axios';
 
-function fetchAPI() {
-  return (
-    <div>
-      
-    </div>
-  )
+function reducer(state, action){
+  switch(action.type){
+    case 'LOADING':
+      return {loading:true, data:null, error:null};
+    case 'SUCCESS':
+      return {loading:false, data:action.data, error:null};
+    case 'ERROR':
+      return {loading:false, data:null, error:action.error};
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
 }
 
-export default fetchAPI;
+export default function FetchAPI(method, URL, payload=null) {
+  const [state, dispatch] = useReducer(reducer, {
+    loading:false,
+    data:null,
+    error:null
+  })
+  
+  
+  useEffect(_=>{
+    const fetchData = async () => {
+      dispatch({type:'LOADING'});
+      try {
+        const response = await axios({
+          url:URL,
+          method:method,
+          data:payload // payload 형식 {firstName: 'Fred',lastName: 'Flintstone'}
+        });
+        dispatch({type:'SUCCESS', data:response.data})
+      } catch (e) {
+        dispatch({type:'ERROR', data:e})
+      }
+    }
+    fetchData();
+  },[method, URL, payload]);
+  return state;
+}
