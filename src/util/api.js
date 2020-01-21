ba289fbeb600c738575470e41e2deeaf2bf0caa2
girin-dev <kw8799@gmail.com>
2020-01-21 13:35:53 +0900

    API통신. GET/POST 두가지 테스트 성공 확인

--- src/util/api.js
+++ src/util/api.js
@@ -12,7 +12,17 @@ export async function getUser(id){
 
 // API 통신할 프로미스 함수 정의
 // (예시)
-//export async function getPosts(){
-//    const res = await axios.get(`http://jsonplaceholder.typicode.com/posts`);
-//    return res.data;
-//}
+// export async function getPosts(){
+//     const res = await axios.get(`http://jsonplaceholder.typicode.com/posts`);
+//     return res.data;
+// }
+
+// export async function postDraft(title, body, userId){
+//     const res = await axios({
+//         method: 'post',
+//         url: 'https://jsonplaceholder.typicode.com/posts',
+//         data: { title:title, body:body, userId:userId },
+//         header: { "Content-type": "application/json; charset=UTF-8" },
+//       });
+//     return res.data;
+// }
