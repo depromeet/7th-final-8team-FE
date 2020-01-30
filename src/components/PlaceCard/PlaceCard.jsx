102b64a4be20879a9bf3f1352538956589b254d0
girin-dev <kw8799@gmail.com>
2020-01-30 21:26:09 +0900

    컴포넌트수정. PlaceCard HTML 구성

--- src/components/PlaceCard/PlaceCard.jsx
+++ src/components/PlaceCard/PlaceCard.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import {styled} from 'styled-components';
 
-function PlaceCard() {
+const ArticleCard = styled.article`
+`;
+const DivPicture = styled.div`
+`;
+const SpanLabel = styled.span`
+`;
+const SpanRating = styled.span`
+`;
+const DivStars = styled.div`
+`;
+const SpanReviews = styled.span`
+`;
+const SpanDistance = styled.span`
+`;
+
+function PlaceCard({ type, name, rating, numberOfReviews, picture, distance }) {
   return (
-    <>
-      
-    </>
+    <ArticleCard>
+      <h2>{name}</h2>
+      <DivPicture></DivPicture>
+      <SpanLabel>{type}</SpanLabel>
+      <SpanRating>{rating}</SpanRating>
+      <DivStars></DivStars>
+      <SpanReviews>리뷰 {numberOfReviews} 건</SpanReviews>
+      <SpanDistance>내 위치에서 {distance}</SpanDistance>
+    </ArticleCard>
   )
 }
 
+PlaceCard.defaultProps = {
+  type:"관광", name:"관광관광", rating:"8.2", numberOfReviews:"7",
+  picture:"https://picsum.photos/seed/picsum/200/300",
+  distance:'10km',
+}
 export default PlaceCard;
