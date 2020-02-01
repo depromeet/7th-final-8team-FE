9e5a7a5c784724c1cdfafb04646df5ec2d5d163d
girin-dev <kw8799@gmail.com>
2020-02-01 18:48:03 +0900

    컴포넌트추가. Header 레이아웃 잡음

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import InputText from '../../components/InputText';
+import Button from '../../components/Button';
+import logoNonoPlan from '../../assets/logoNonoPlan.gif';
 
+const HeaderContainer = styled.header`
+  height:88px;
+  display:flex;
+  justify-content:space-around;
+`;
+const ImgLogo = styled.img`
+  height: 24px;
+  margin:auto 0;
+`;
+const DivSearchInput = styled.div`
+  left:0;
+  right:0;
+  display:inline-block;
+  width:100%;
+  margin:auto 0;
+  vertical-align:middle;
+`;
 function Header() {
   return (
-    <>
-      
-    </>
+    <HeaderContainer>
+        <ImgLogo src={logoNonoPlan} alt="Nonoplan 로고"/>
+        <DivSearchInput>
+          <InputText isLabel={false} style={{minWidth:"100px",maxWidth:"750px",width:"100%",
+    margin:"0 auto"}}/>
+        </DivSearchInput>
+        <Button color="#764ef5" style={{margin:"auto 0"}}>로그인</Button>
+    </HeaderContainer>
   )
 }
 
