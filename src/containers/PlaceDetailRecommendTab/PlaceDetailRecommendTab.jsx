ea2512f668221146e81e1ee278576a81d6d95ee2
girin-dev <kw8799@gmail.com>
2020-02-12 09:00:18 +0900

    컴포넌트추가. 장소상세추천탭 추가

--- src/containers/PlaceDetailRecommendTab/PlaceDetailRecommendTab.jsx
+++ src/containers/PlaceDetailRecommendTab/PlaceDetailRecommendTab.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import RecommendedPlace from '../../components/RecommendedPlace';
+
+const initialData = [
+  {type:'관광', name:'관광1', rating:3.5, numberOfReviews:3, km:13, picture:'https://picsum.photos/200'},
+  {type:'맛집', name:'맛집1', rating:3, numberOfReviews:145, km:11, picture:'https://picsum.photos/200'},
+  {type:'카페', name:'카페2', rating:4, numberOfReviews:2, km:10, picture:'https://picsum.photos/200'},
+  {type:'관광', name:'관광1', rating:3.2, numberOfReviews:0, km:2, picture:'https://picsum.photos/200'},
+]
 
 function PlaceDetailRecommendTab() {
   return (
     <>
-      
+      {
+        initialData.map(placeData=><RecommendedPlace placeData={placeData} size="small"/>)
+      }
     </>
   )
 }
