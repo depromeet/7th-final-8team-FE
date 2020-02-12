bc9e39568a96b827ad6875225ca6da9827746837
girin-dev <kw8799@gmail.com>
2020-02-12 21:24:55 +0900

    컴포넌트추가. 장소상세 인포탭과 내비탭 작업

--- src/containers/PlaceDetail/PlaceDetail.jsx
+++ src/containers/PlaceDetail/PlaceDetail.jsx
@@ -1,11 +1,135 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function PlaceDetail() {
+const ArticlePlaceDetail = styled.article`
+  align-items: center; 
+  border-radius:20px;
+  box-shadow: 0px 3px 5px rgba(196, 196, 196, 0.2), 0px 1px 18px rgba(34, 34, 34, 0.12), 0px 6px 10px rgba(196, 196, 196, 0.14);
+  width:800px;
+  margin: 0 auto;
+  padding: 40px 20px;
+`;
+
+const DivInfoTab = styled.div`
+  text-align:center;
+  width:100%;
+  line-height:42px;
+  span {
+    /* 타입 */
+    font-weight: bold;
+    font-size: 24px;
+    letter-spacing: -0.55px;
+    color: #764EF5;
+  }
+  h1{
+    /* 이름 */
+    font-weight: bold;
+    font-size: 32px;
+    letter-spacing: -0.55px;
+    color: #191919;
+  }
+  div {
+    /* 거리 */
+    font-size: 18px;
+    letter-spacing: -0.55px;
+    color: #5F5F5F;
+  }
+`;
+
+const DivStarReviewCnt = styled.div`
+  margin: 0 auto;
+  div {
+    display:inline-block;
+    position:relative;
+    padding:0 10px;
+  }
+  div + div:before {
+    /* 짧은 border를 만들기 위한 가상요소 */
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 50%;
+    transform: translateY(40%);
+    height: 17px;
+    width: 1px;  /* or 100px */
+    border-left:1px solid #191919;
+  }
+  span {
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 27px;
+    letter-spacing: -0.25px;
+  }
+`;
+
+const NavTabs = styled.nav`
+  ul{
+    display:flex;
+  }
+  li {
+    flex:1;
+    flex-basis:0;
+  }
+  button {
+    width:100%;
+    height:51px;
+    background:none;
+    border:0;
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 27px;
+    letter-spacing: -0.25px;
+    cursor: pointer;
+  }
+  button:focus, button:active{
+    background:none;
+    border:0;
+    outline:none;
+    box-shadow:0 1px #191919;
+  }
+`;
+
+function PlaceDetail({type, name, rating, numberOfReviews, km,}) {
   return (
-    <>
-      
-    </>
+    <ArticlePlaceDetail>
+      <DivInfoTab>
+        <span>{type}</span>
+        <h1>{name}</h1>
+        <DivStarReviewCnt>
+          {/* 별점 */}
+          <div>
+            <span>{rating}</span>
+          </div>
+          {/* 리뷰수 */}
+          <div>리뷰 <span>{numberOfReviews}</span>개</div>
+        </DivStarReviewCnt>
+        <div>내 위치에서 {km}km</div>
+      </DivInfoTab>
+      <NavTabs>
+        <ul>
+          <li>
+            {/* 정보 탭 버튼 */}
+            <button>정보</button>
+          </li>
+          <li>
+            {/* 리뷰 탭 버튼 */}
+            <button>리뷰</button>
+          </li>
+          <li>
+            {/* 추천 탭 버튼 */}
+            <button>추천</button>
+          </li>
+        </ul>
+      </NavTabs>
+    </ArticlePlaceDetail>
   )
+}
+PlaceDetail.defaultProps = {
+  type:"관광",
+  name:"와인키키절벽",
+  rating:"3.9",
+  numberOfReviews:"7",
+  km:"10"
 }
 
 export default PlaceDetail;
