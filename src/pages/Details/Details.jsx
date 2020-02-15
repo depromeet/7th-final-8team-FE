7e59cac19bac2d119068bb6b83757fd620be1ca2
girin-dev <kw8799@gmail.com>
2020-02-15 14:49:53 +0900

    Detail페이지 1차 조립

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../../containers/Header/Header'
+import PlaceDetail from '../../containers/PlaceDetail/PlaceDetail';
 
 function Details() {
   return (
     <>
+      <Header></Header>
+      <PlaceDetail></PlaceDetail>
     </>
   );
 }
