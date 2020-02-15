bf1810ea074542f47cd9142d95b6f626ef23aea5
girin-dev <kw8799@gmail.com>
2020-02-15 15:32:51 +0900

    컴포넌트수정. 기본데이터 및 카드 입력

--- src/containers/PlaceDetailRecommendTab/PlaceDetailRecommendTab.jsx
+++ src/containers/PlaceDetailRecommendTab/PlaceDetailRecommendTab.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import RecommendedPlace from '../../components/RecommendedPlace';
 
-function PlaceDetailRecommendTab() {
+function PlaceDetailRecommendTab({datas}) {
+  const cards = datas.map((data,idx)=>(<RecommendedPlace size="small" placeData={data} key={idx}/>))
   return (
     <>
-      
+    {cards}
     </>
   )
 }
 
+PlaceDetailRecommendTab.defaultProps = {
+  datas: [
+    { type:"관광", name:"와인키키절벽", rating:"3.9", numberOfReviews:"7", km:"10" },
+    { type:"관광", name:"와인키키절벽", rating:"3.9", numberOfReviews:"7", km:"10" },
+    { type:"관광", name:"와인키키절벽", rating:"3.9", numberOfReviews:"7", km:"10" },
+    { type:"관광", name:"와인키키절벽", rating:"3.9", numberOfReviews:"7", km:"10" },
+    { type:"관광", name:"와인키키절벽", rating:"3.9", numberOfReviews:"7", km:"10" },
+    { type:"관광", name:"와인키키절벽", rating:"3.9", numberOfReviews:"7", km:"10" },
+  ]
+}
 export default PlaceDetailRecommendTab;
