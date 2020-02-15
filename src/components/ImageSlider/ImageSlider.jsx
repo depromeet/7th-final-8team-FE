0b6d2f9256e1544146dbe52052fe7e19d9c7550e
girin-dev <kw8799@gmail.com>
2020-02-15 12:36:42 +0900

    컴포넌트추가. ImageSlider의 이미지 아이템 추가

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const DivImageItem = styled.div`
+  width:420px;
+  height:420px;
+  float:left;
+  background: url(${props => props.url ? props.url : "https://picsum.photos/420/420"});
+`;
 
 function ImageSlider() {
+  // 사진 랜덤 제공 url
+  // https://picsum.photos/420/420
+  const imgUrlList = [
+    "https://picsum.photos/420/420",
+    "https://picsum.photos/420/420",
+    "https://picsum.photos/420/420",
+    "https://picsum.photos/420/420",
+    "https://picsum.photos/420/420",
+  ];
+  const imgSlider = imgUrlList.map(url=><DivImageItem url={url}/>);
   return (
     <>
-      
+      {imgSlider}
     </>
   )
 }
