32d1c875678a7bc1573a9e299c3e2d7fb69ee40c
girin-dev <kw8799@gmail.com>
2020-02-17 00:18:41 +0900

    컴포넌트수정. 이미지 슬라이더 기본기능 커스텀 적용

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -1,38 +1,61 @@
-import React from 'react';
+import React, { useRef } from 'react'
 import styled from 'styled-components';
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
 
-const LiImageItem = styled.li`
-    width:420px;
-  div {
-    height:420px;
-    background: url(${props => props.url ? props.url : "https://picsum.photos/420/420"});
-  }
-`;
-
-const UlImageList = styled.ul`
-  /* overflow:hidden; */
+const DivContainer = styled.div`
+  position:relative;
   height:420px;
-  display: flex;
-  flex-direction: column;
-  justify-content:center;
+  width:2100px;
+  margin:0 auto;
 `;
 
-function ImageSlider() {
-  // 사진 랜덤 제공 url
-  // https://picsum.photos/420/420
-  const imgUrlList = [
-    "https://picsum.photos/420/420",
-    "https://picsum.photos/420/420",
-    "https://picsum.photos/420/420",
-    "https://picsum.photos/420/420",
-    "https://picsum.photos/420/420",
-  ];
-  const imgSlider = imgUrlList.map((url,idx)=><LiImageItem key={idx}><div></div></LiImageItem>);
-  return (
-    <UlImageList>
-      {imgSlider}
-    </UlImageList>
-  )
+const DivImageItem = styled.div`
+  width:420px;
+  height:420px;
+  display:inline-block;
+  background: url(${props => !props.url ? "" : props.url});
+`;
+
+const ImageSlider = ({datas}) =>{
+  let arrInit = [null,null,null,null,null];
+  let sliderItems;
+  if (datas.length < 5) {
+    sliderItems = arrInit.map((item,idx)=>!datas[idx] ? datas[idx-datas.length] : datas[idx])
+  } else {
+    sliderItems = datas;
+  }
+  console.log('sliderItems',sliderItems)
+  const handleOnDragStart = (e) => e.preventDefault()
+  const imgSlider = sliderItems.map((item,idx)=><DivImageItem key={idx} onDragStart={handleOnDragStart} url={item}>{idx}</DivImageItem>)
+  const carousel = useRef();
+    return (
+      <DivContainer>
+        <AliceCarousel
+          mouseTrackingEnabled
+          items={imgSlider}
+          responsive={ {0: { items: 5 }} }
+          dotsDisabled={true}
+          ref={carousel}
+        />
+        <button onClick={() => carousel.current.slidePrev()}>Prev button</button>
+        <button onClick={() => carousel.current.slideNext()}>Next button</button>
+      </DivContainer>
+    )
 }
+
+ImageSlider.defaultProps = {
+  datas: [
+    "https://picsum.photos/420/420",
+    "https://picsum.photos/420/421",
+    "https://picsum.photos/420/422",
+    "https://picsum.photos/420/423",
+    "https://picsum.photos/420/424",
+    // "https://picsum.photos/420/420",
+    // "https://picsum.photos/420/420",
+    // "https://picsum.photos/420/420",
+    // "https://picsum.photos/420/420",
+  ]
+};
 
 export default ImageSlider;
