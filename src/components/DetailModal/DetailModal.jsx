6fb03c23426f86bbbdc1920dee0ddcda3549aaf7
girin-dev <kw8799@gmail.com>
2020-02-18 20:40:14 +0900

    컴포넌트수정. section body의 padding 제거

--- src/components/DetailModal/DetailModal.jsx
+++ src/components/DetailModal/DetailModal.jsx
@@ -110,9 +110,9 @@ function DetailModal({infoHeader,tabList, tabBodyList, isMyPage}) {
       <NavTabs>
         {genTab(tabList)}
       </NavTabs>
-      <SectionTabBody isMyPage={isMyPage}>
+      <section>
         {tabBody}
-      </SectionTabBody>
+      </section>
     </ArticlePlaceDetail>
   )
 };
