a9573ed8f80840aee3c4e179ef3a7476c36246ec
girin-dev <kw8799@gmail.com>
2020-02-21 23:50:41 +0900

    클릭한 장소로 지도 중앙이동

--- src/containers/KakaoMapContainer.jsx
+++ src/containers/KakaoMapContainer.jsx
@@ -1,24 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import KakaoMap from '../components/KakaoMap';
 import { useDataState } from '../util/DataContext';
 import { useEffect } from 'react';
-
-function KakaoMapContainer({ clickedId, defalutCenter, defaultMarkers }) {
+import {CenterIdContext} from '../pages/Home/index';
+function KakaoMapContainer({ defalutCenter, defaultMarkers }) {
   // Context API에서 검색한 데이터를 받아옴
   const state = useDataState();
   const {loading, data:locations, error} = state.locations;
 
   const [center, setCenter] = useState(defalutCenter);
   const [markers, setMarkers] = useState(defaultMarkers);
-  const _site = !locations ? defaultMarkers : locations.content.map(item=>{return {id:item.id, lat:item.latitude, lng:item.longitude }})
+  // const _site = !locations ? defaultMarkers : locations.content.map(item=>{return {id:item.locationId, lat:item.latitude, lng:item.longitude }})
   // 현재 그냥 첫번째를 찍어주지만 향후 검색결과들의 중간쯤의 위치로 변경할 예정
-  useEffect(_=>{setCenter(_site[0])},[locations])
-  useEffect(_=>{setMarkers(_site)},[locations])
-  if(!!clickedId) {
-    const centerLatLng = markers.filter(marker=>marker.id === clickedId)[0];
-    setCenter(centerLatLng);
-    console.log('clickedId가 있을 때',center);
-  }
+  useEffect(_=>{setCenter((!locations ? defaultMarkers : locations.content.map(item=>{return {id:item.locationId, lat:item.latitude, lng:item.longitude }}))[0])},[locations, defaultMarkers])
+  useEffect(_=>{setMarkers(!locations ? defaultMarkers : locations.content.map(item=>{return {id:item.locationId, lat:item.latitude, lng:item.longitude }}))},[locations,defaultMarkers])
+  
+  // RecommenedPlace를 클릭해 받아온 centerId 값이 있는지에 따라 KakaoMap 리렌더
+  const clickedId = useContext(CenterIdContext).centerId;
+  console.log('clickedId',clickedId)
+  useEffect(_=>{if(!clickedId) {
+    console.log('clickedId',clickedId)
+    setCenter(defalutCenter)
+    console.log('click 하지 않은 상태')
+    console.log('center',center)
+  } else {
+    console.log('clickedId',clickedId)
+    setCenter(markers.filter(marker=>marker.id === clickedId)[0])
+    console.log('click 한 상태')
+    console.log('center',center)
+    }
+  },[markers,defalutCenter,clickedId])
   return (
     <>
       <KakaoMap datas={markers} centerLatLng={center}/>
